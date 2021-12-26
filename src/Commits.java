928d7d4a60247365e2ed779edad6949106fdd79b
VanyushinRoman <rayman1991@bk.ru>
2021-12-26 16:07:43 +0300

    Commit 1.2

--- src/Commits.java
+++ src/Commits.java
@@ -1,13 +1,13 @@
 public class Commits {
-    private int[] nums;
+    private double[] nums;
 
 
-    public Commits(int[] nums) {
+    public Commits(double[] nums) {
         this.nums = nums;
     }
 
-    public int getMax() {
-        int rezult = nums[0];
+    public double getMax() {
+        double rezult = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > rezult)
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 10, 0, -6};
+        double[] nums = {1, 10, 0, -6};
 
         Commits commits = new Commits(nums);
 
